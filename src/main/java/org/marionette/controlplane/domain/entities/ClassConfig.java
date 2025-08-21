57f7659b4eeca4668aa953de3888d8b5977c2a07
Matteo <matteo.sissa29@gmail.com>
2025-08-21 11:42:22 +0200

    feat(classConfig): modelled aggregate relationship and accessors for internal data for method config

--- src/main/java/org/marionette/controlplane/domain/entities/ClassConfig.java
+++ src/main/java/org/marionette/controlplane/domain/entities/ClassConfig.java
@@ -1,12 +1,14 @@
 package org.marionette.controlplane.domain.entities;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.HashMap;
+
+import org.marionette.controlplane.domain.values.BehaviourId;
 import org.marionette.controlplane.domain.values.ClassName;
+import org.marionette.controlplane.domain.values.MethodName;
 
 /**
  * Does not contain duplicate elements following the convention on equality expressed by MethodConfig equals() method 
@@ -15,32 +17,42 @@ import org.marionette.controlplane.domain.values.ClassName;
 public class ClassConfig {
 
     private final ClassName className;
-    private final Set<MethodConfig> methodsConfig = new HashSet<>();  // Aggregate relationship
+    private final Map<MethodName, MethodConfig> methodsConfig = new HashMap<>();  // Aggregate relationship between class config and method config
     
-    public ClassConfig(ClassName className, List<MethodConfig> methodsConfig) {
-
+    public ClassConfig(ClassName className) {
         this.className = requireNonNull(className, "Trying to build a ClassConfig object with null className");
-        this.methodsConfig.addAll(requireNonNull(methodsConfig, "Trying to build a ClassConfig object with a null reference for the method configurations set"));
-    
     }
 
-    public void addMethodConfig(MethodConfig methodConfig) {
+    public void addMethodConfig(MethodName methodName, MethodConfig methodConfig) {
         requireNonNull(methodConfig, "Trying to add a null MethodConfig object inside a ClassConfig");
-        methodsConfig.add(methodConfig);
+        requireNonNull(methodName, "Trying to add a method configuration with a null name to the ClassConfig object");
+        methodsConfig.put(methodName, methodConfig);
     }
 
-    public void removeMethodConfig(MethodConfig methodConfig) {
-        requireNonNull(methodConfig, "Trying to remove a null MethodConfig object inside a ClassConfig");
-        methodsConfig.remove(methodConfig);
+    public void addAll(Map<MethodName, MethodConfig> configurations) {
+        requireNonNull(configurations, "Trying to add configurations to a ClassConfig object with a null map");
+        methodsConfig.putAll(configurations);
+    }
+
+    public void removeMethodConfig(MethodName methodName) {
+        requireNonNull(methodName, "Trying to remove a MethodConfig object inside a ClassConfig with a null MethodName reference");
+        methodsConfig.remove(methodName);
+    }
+
+    public void modifyCurrentBehaviour(MethodName method, BehaviourId newBehaviour) {
+        requireNonNull(method, "The method name cannot be null");
+        requireNonNull(newBehaviour, "The newBehaviour cannot be null");
+
+        if(!methodsConfig.containsKey(method)) {
+            throw new IllegalArgumentException("The method with name " + method.getMethodName() + " does not exist in the current class configuration for the class " + className.getClassName());
+        }
+
+        methodsConfig.get(method).setCurrentBehaviourId(newBehaviour);
     }
 
     public ClassName getClassName() {
         return className;
     }
-
-
-
-
 
     
 }
