9a9bdf63237a3966b60adda65c5cb40c93c9444c
Matteo <matteo.sissa29@gmail.com>
2025-08-23 18:52:04 +0200

    feat(finished use case to add service config): injected factory into the use case, finished with implementaiton of the use case

--- src/main/java/org/marionette/controlplane/usecases/addserviceconfig/AddServiceConfigUseCase.java
+++ src/main/java/org/marionette/controlplane/usecases/addserviceconfig/AddServiceConfigUseCase.java
@@ -3,23 +3,25 @@ package org.marionette.controlplane.usecases.addserviceconfig;
 import java.util.Objects;
 
 import org.marionette.controlplane.domain.entities.ConfigRegistry;
+import org.marionette.controlplane.domain.entities.ServiceConfig;
 import org.marionette.controlplane.usecases.addserviceconfig.request.AddServiceConfigRequest;
 import org.marionette.controlplane.usecases.ports.in.AddServiceConfigPort;
 
 public class AddServiceConfigUseCase implements AddServiceConfigPort {
 
     private final ConfigRegistry globalRegistry;
+    private final ServiceConfigFactory serviceConfigFactory;
 
-    public AddServiceConfigUseCase(ConfigRegistry globalRegistry) {
+    public AddServiceConfigUseCase(ConfigRegistry globalRegistry, ServiceConfigFactory serviceConfigFactory) {
         this.globalRegistry = globalRegistry;
+        this.serviceConfigFactory = serviceConfigFactory;
     }
 
     @Override
     public void handle(AddServiceConfigRequest request) {
         Objects.requireNonNull(request, "The request object cannot be null");
-
-        globalRegistry.addServiceConfig(request.getServiceName(), request.getServiceConfig());
-
+        ServiceConfig serviceConfigToAdd = serviceConfigFactory.createServiceConfig(request.serviceName(), request.classConfigData());
+        globalRegistry.addServiceConfig(serviceConfigToAdd.getServiceName(), serviceConfigToAdd);
     }
     
 }
