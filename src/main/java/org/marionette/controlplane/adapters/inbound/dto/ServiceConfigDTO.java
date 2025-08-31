b4bf7386a3e6ae7486c2c0538286fd70c91df584
Matteo <matteo.sissa29@gmail.com>
2025-08-31 20:25:38 +0200

    feat(DTO): modified DTOs into record classes

--- src/main/java/org/marionette/controlplane/adapters/inbound/dto/ServiceConfigDTO.java
+++ src/main/java/org/marionette/controlplane/adapters/inbound/dto/ServiceConfigDTO.java
@@ -1,21 +1,5 @@
 package org.marionette.controlplane.adapters.inbound.dto;
 
-import java.util.Map;
+import java.util.List;
 
-public class ServiceConfigDTO {
-    private String serviceName;
-    private Map<String, ClassConfigDTO> classes;
-
-    public ServiceConfigDTO() {}
-    
-    public ServiceConfigDTO(String serviceName, Map<String, ClassConfigDTO> classes) {
-        this.serviceName = serviceName;
-        this.classes = classes;
-    }
-
-    public String getServiceName() { return serviceName; }
-    public void setServiceName(String serviceName) { this.serviceName = serviceName; }
-    
-    public Map<String, ClassConfigDTO> getClasses() { return classes; }
-    public void setClasses(Map<String, ClassConfigDTO> classes) { this.classes = classes; }
-}
+public record ServiceConfigDTO (String serviceName, List<ClassConfigDTO> classConfigs) {}
