b9b6bd4f7e40b3e86b39578b791357ad8a3ccbd2
Matteo <matteo.sissa29@gmail.com>
2025-09-03 17:55:56 +0200

    feat(dto for prometheus): designed DTO classes to work with Prometheus API

--- src/main/java/org/marionette/controlplane/adapters/outbound/fetchmetrics/prometheus/PrometheusFetchMarionetteNodesMetricsAdapter.java
+++ src/main/java/org/marionette/controlplane/adapters/outbound/fetchmetrics/prometheus/PrometheusFetchMarionetteNodesMetricsAdapter.java
@@ -1,26 +1,56 @@
 package org.marionette.controlplane.adapters.outbound.fetchmetrics.prometheus;
 
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpClient.Version;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.marionette.controlplane.adapters.outbound.fetchmetrics.prometheus.domain.PrometheusMetricConfig;
 import org.marionette.controlplane.usecases.outbound.fetchmetrics.FetchMarionetteNodesMetricsGateway;
 import org.marionette.controlplane.usecases.outbound.fetchmetrics.domain.AggregateMetric;
 
 public class PrometheusFetchMarionetteNodesMetricsAdapter implements FetchMarionetteNodesMetricsGateway {
 
     private final PrometheusConfiguration config;
+    private final HttpClient httpClient;
 
-    PrometheusFetchMarionetteNodesMetricsAdapter(PrometheusConfiguration config) {
+    public PrometheusFetchMarionetteNodesMetricsAdapter(PrometheusConfiguration config) {
+        this.httpClient = createHttpClient();
         this.config = config;
+    }
+
+    private HttpClient createHttpClient() {
+        return HttpClient.newBuilder()
+                .version(Version.HTTP_1_1)
+                .followRedirects(Redirect.NORMAL)
+                .connectTimeout(Duration.ofSeconds(10))
+                .build();
     }
 
     @Override
     public List<AggregateMetric> fetchMetricsForService(String serviceName, Duration timeSpan) {
-        
+
+        List<AggregateMetric> metrics = new ArrayList<>();
+
+        for (PrometheusMetricConfig metricConfig : config.getMetrics()) {
+
+            // Build the API url
+            String query = PrometheusQueryBuilder.buildAggregationQuery(
+                    config.getUrl(),
+                    config.getApiPath(),
+                    serviceName,
+                    metricConfig,
+                    timeSpan);
+
+            // Fire the query
 
 
 
+
+        }
 
     }
-    
+
 }
